e6d7176348b06839e5827590a4322106af824a85
Fouink <yyss9103@gmail.com>
2023-08-01 22:05:28 +0900

    회원 조회, 글로벌 익셉션 추가

---
+++ src/main/java/com/example/product/common/exception/BadRequestException.java
@@ -0,0 +1,28 @@
+package com.example.product.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException {
+    private final String errorCode;
+
+    public BadRequestException(String errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public BadRequestException(String message, Throwable cause) {
+        super(message, cause);
+        this.errorCode = "";
+    }
+
+    public BadRequestException(BadRequestException e) {
+        super(e.getMessage());
+        this.errorCode = e.getErrorCode();
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+}
