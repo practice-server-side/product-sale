c19ccbd7b40786cfab5c0c622b1702c2980aa338
Fouink <yyss9103@gmail.com>
2023-08-08 23:03:13 +0900

    gateway oauth 라우팅 추가 및 수정, 회원 가입 구현

--- server-oauth/src/main/java/com/example/product/api/controller/OauthController.java
+++ server-oauth/src/main/java/com/example/product/api/controller/OauthController.java
@@ -1,6 +1,9 @@
 package com.example.product.api.controller;
 
+import com.example.product.api.dto.CustRegisterRequestDto;
 import com.example.product.api.dto.LoginRequestDto;
+import com.example.product.api.model.Cust;
+import com.example.product.api.repository.CustRepository;
 import com.example.product.config.ApiAuthenticationProvider;
 import com.example.product.exception.UnAuthorizationException;
 import lombok.RequiredArgsConstructor;
@@ -10,10 +13,15 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +29,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/server-oauth/v1")
 public class OauthController {
     private final ApiAuthenticationProvider authenticationManager;
+    private final CustRepository custRepository;
+    private final PasswordEncoder passwordEncoder;
+
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequestDto request) {
         try {
@@ -34,5 +45,26 @@ public class OauthController {
         } catch (AuthenticationException e) {
             throw new UnAuthorizationException("", ""); //TODO : 예외문구
         }
+    }
+
+    /**
+     *  고객 회원 가입
+     */
+    @PostMapping("/join")
+    public ResponseEntity<?> custRegister(@RequestBody CustRegisterRequestDto request) {
+
+        URI selfLink = URI.create(ServletUriComponentsBuilder.fromCurrentRequest().toUriString());
+
+        Cust newCust = Cust.builder()
+                .loginId(request.getLoginId())
+                .loginPassword(passwordEncoder.encode(request.getLoginPassword()))
+                .userName(request.getUserName())
+                .userPhone(request.getUserPhone())
+                .clientKey(UUID.randomUUID().toString())
+                .build();
+
+        custRepository.save(newCust);
+
+        return ResponseEntity.created(selfLink).build();
     }
 }
