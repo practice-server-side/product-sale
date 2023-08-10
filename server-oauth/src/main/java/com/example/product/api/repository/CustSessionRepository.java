1a7910c48b9eeece9be80d47677a7b63f5775701
Fouink <yyss9103@gmail.com>
2023-08-10 22:13:18 +0900

    로그인 세션 redis 저장

---
+++ server-oauth/src/main/java/com/example/product/api/repository/CustSessionRepository.java
@@ -0,0 +1,8 @@
+package com.example.product.api.repository;
+
+import com.example.product.api.model.CustSession;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustSessionRepository extends CrudRepository<CustSession, Long> {
+
+}
