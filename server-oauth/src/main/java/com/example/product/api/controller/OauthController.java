c391b8f4a7896d6687a42dc95cba50d9cc03256f
Fouink <yyss9103@gmail.com>
2023-08-10 22:41:10 +0900

    스프링 기본 세션 레디스 저장 로직으로 변경

--- server-oauth/src/main/java/com/example/product/api/controller/OauthController.java
+++ server-oauth/src/main/java/com/example/product/api/controller/OauthController.java
@@ -4,17 +4,15 @@ import com.example.product.api.dto.CustRegisterRequestDto;
 import com.example.product.api.dto.LoginRequestDto;
 import com.example.product.api.dto.LoginResponseDto;
 import com.example.product.api.model.Cust;
-import com.example.product.api.model.CustSession;
 import com.example.product.api.repository.CustRepository;
-import com.example.product.api.repository.CustSessionRepository;
 import com.example.product.config.ApiAuthenticationProvider;
-import com.example.product.exception.UnAuthorizationException;
+import com.example.product.dto.CurrentCust;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,33 +32,27 @@ public class OauthController {
     private final ApiAuthenticationProvider authenticationManager;
     private final CustRepository custRepository;
     private final PasswordEncoder passwordEncoder;
-    private final CustSessionRepository custSessionRepository;
-
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequestDto request) {
-        try {
-            Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(request.getLoginId(), request.getLoginPassword())
-            );
+    public ResponseEntity<?> login(@RequestBody LoginRequestDto request, HttpSession session) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getLoginId(), request.getLoginPassword()));
 
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
-            CustSession custSession = CustSession.builder()
-                    .custId((Long) authentication.getCredentials())
-                    .session("TODO : randomStr")
-                    .build();
+        CurrentCust sessionValue = CurrentCust.builder()
+                .custId((Long) authentication.getCredentials())
+                .build();
 
-            custSessionRepository.save(custSession);
+        session.setAttribute("user", sessionValue);
 
-            return ResponseEntity.ok(
-                    LoginResponseDto.builder()
-                            .session(custSession.getSession())
-                            .build()
-            );
-        } catch (AuthenticationException e) {
-            throw new UnAuthorizationException("C02", "C02");
-        }
+        System.out.println("session.getId() = " + session.getId());
+
+        return ResponseEntity.ok(
+                LoginResponseDto.builder()
+                        .session(session.getId())
+                        .build()
+        );
     }
 
     /**
---
+++ server-oauth/src/main/java/com/example/product/config/HttpSessionConfig.java
@@ -0,0 +1,7 @@
+package com.example.product.config;
+
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@EnableRedisHttpSession
+public class HttpSessionConfig {
+}
