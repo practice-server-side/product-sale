c756a6b94690f44122ac77f48704b5310260ada7
Fouink <yyss9103@gmail.com>
2023-09-05 21:58:47 +0900

    MallMemberAction 모듈 서버 추가

---
+++ server-mallMemberAction/src/test/java/com/example/servermallmemberaction/ServerMallMemberActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servermallmemberaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerMallMemberActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
